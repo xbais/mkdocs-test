2a4011d29091bcee4fd371382814778a769fba5e
Aakash Singh Bais <aakash@aioniaepochi.org>
2023-12-14 18:37:23 +0530

    Add project code and documentation

---
+++ test/__init__.py
@@ -0,0 +1,10 @@
+# calculator/__init__.py
+
+"""Do math with your own functions.
+
+Modules exported by this package:
+
+- `calculations`: Provide several sample math calculations.
+"""
+
+import test
---
+++ test/test.py
@@ -0,0 +1,31 @@
+# test/test.py
+
+"""Provide several sample math calculations.
+
+This module allows the user to make mathematical calculations.
+
+Examples:
+    >>> from calculator import calculations
+    >>> calculations.add(2, 4)
+    6.0
+    >>> calculations.multiply(2.0, 4.0)
+    8.0
+    >>> from calculator.calculations import divide
+    >>> divide(4.0, 2)
+    2.0
+
+The module contains the following functions:
+
+- `add(a, b)` - Returns the sum of two numbers.
+- `subtract(a, b)` - Returns the difference of two numbers.
+- `multiply(a, b)` - Returns the product of two numbers.
+- `divide(a, b)` - Returns the quotient of two numbers.
+"""
+
+def add(a, b):
+    """
+    Compute something
+    """
+    return a+b
+
+
